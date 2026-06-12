7a3f60cbc4dc61a10deebea381bbdd946f1ee457
tomikaubrecht-code <tomik.aubrecht@gmail.com>
2026-06-12 15:54:49 +0200

    Update projekt_3.py

--- projekt_3.py
+++ projekt_3.py
@@ -13,7 +13,6 @@ import requests
 from bs4 import BeautifulSoup
 
 def zkontroluj_argumenty():
-    """Zkontroluje, zda uživatel zadal správný počet argumentů a platný odkaz."""
     if len(sys.argv) != 3:
         print("CHYBA: Špatný počet argumentů.")
         print("Spusťte program ve formátu: python projekt_3.py <odkaz_na_uzemni_celek> <jmeno_souboru.csv>")
@@ -27,23 +26,19 @@ def zkontroluj_argumenty():
     return sys.argv[1], sys.argv[2]
 
 def ziskej_soup(url):
-    """Stáhne HTML z URL a vrátí BeautifulSoup objekt."""
     try:
         odpoved = requests.get(url)
         odpoved.raise_for_status()
-        # Využití response.content zabrání problémům s českou diakritikou
         return BeautifulSoup(odpoved.content, "html.parser")
     except requests.exceptions.RequestException as e:
         print(f"CHYBA: Nepodařilo se připojit k zadané URL: {e}")
         sys.exit(1)
 
 def ziskej_odkazy_obci(hlavni_url):
-    """Získá kódy, názvy a URL odkazy jednotlivých obcí z hlavního odkazu."""
     soup = ziskej_soup(hlavni_url)
     zakladni_url = "https://volby.cz/pls/ps2017nss/"
     obce = []
 
-    # Hledání buněk tabulky s třídou 'cislo', které obsahují odkazy na obce
     bunky_s_kody = soup.find_all("td", class_="cislo")
     
     for bunka in bunky_s_kody:
@@ -51,7 +46,6 @@ def ziskej_odkazy_obci(hlavni_url):
         if odkaz_element:
             kod_obce = odkaz_element.text
             odkaz_obce = zakladni_url + odkaz_element["href"]
-            # Název obce je v následující buňce
             nazev_obce = bunka.find_next_sibling("td").text
             
             obce.append({
@@ -63,10 +57,8 @@ def ziskej_odkazy_obci(hlavni_url):
     return obce
 
 def ziskej_data_z_obce(url_obce):
-    """Ze stránky konkrétní obce vytáhne statistiky a hlasy pro strany."""
     soup = ziskej_soup(url_obce)
     
-    # Základní statistiky: voliči, obálky, platné hlasy
     volici = soup.find("td", headers="sa2").text.replace('\xa0', '')
     obalky = soup.find("td", headers="sa3").text.replace('\xa0', '')
     platne_hlasy = soup.find("td", headers="sa6").text.replace('\xa0', '')
@@ -77,8 +69,6 @@ def ziskej_data_z_obce(url_obce):
         "Platné hlasy": platne_hlasy
     }
     
-    # Hlasy pro politické strany
-    # Strany jsou uloženy v tabulkách, kde má název strany typicky třídu 'overflow_name'
     nazvy_stran = soup.find_all("td", class_="overflow_name")
     hlasy_stran = soup.find_all("td", headers=["t1sa2 t1sb3", "t2sa2 t2sb3"])
     
@@ -88,24 +78,20 @@ def ziskej_data_z_obce(url_obce):
     return data_obce
 
 def main():
-    # 1. Kontrola vstupů
     url, jmeno_souboru = zkontroluj_argumenty()
     
     print(f"STAHUJI DATA Z VYBRANÉHO ODKAZU: {url}")
     
-    # 2. Získání seznamu obcí
     obce = ziskej_odkazy_obci(url)
     if not obce:
         print("CHYBA: Na zadané adrese nebyly nalezeny žádné obce. Zkontrolujte odkaz.")
         sys.exit(1)
-        
-    # 3. Stažení dat z první obce pro vytvoření hlavičky CSV
+
     prvni_obec_data = ziskej_data_z_obce(obce[0]["odkaz"])
-    strany = list(prvni_obec_data.keys())[3:] # Názvy stran (od 4. klíče dál)
+    strany = list(prvni_obec_data.keys())[3:]
     
     hlavicka = ["Kód obce", "Název obce", "Voliči v seznamu", "Vydané obálky", "Platné hlasy"] + strany
-    
-    # 4. Sběr dat a zápis do CSV
+
     print(f"UKLÁDÁM DATA DO SOUBORU: {jmeno_souboru}")
     
     with open(jmeno_souboru, mode="w", newline="", encoding="utf-8-sig") as csv_soubor:
@@ -113,7 +99,6 @@ def main():
         writer.writerow(hlavicka)
         
         for obec in obce:
-            # print(f"Zpracovávám: {obec['nazev']}...") # Volitelné: odkomentuj pro sledování průběhu
             data = ziskej_data_z_obce(obec["odkaz"])
             
             radek = [
@@ -124,7 +109,6 @@ def main():
                 data["Platné hlasy"]
             ]
             
-            # Přidání hlasů pro jednotlivé strany
             for strana in strany:
                 radek.append(data.get(strana, "0"))
                 
